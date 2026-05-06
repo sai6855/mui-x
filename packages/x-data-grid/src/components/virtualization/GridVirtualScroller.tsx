4354340c7fff4717dbb2aabc1356e330b7206596
sai6855 <fayazceg@gmail.com>
2026-05-06 14:54:11 +0530

    [data grid] Fix overscroll should be disabled when pinning columns

--- packages/x-data-grid/src/components/virtualization/GridVirtualScroller.tsx
+++ packages/x-data-grid/src/components/virtualization/GridVirtualScroller.tsx
@@ -34,6 +34,7 @@ import { useGridVirtualizer } from '../../hooks/core/useGridVirtualizer';
 type OwnerState = Pick<DataGridProcessedProps, 'classes'> & {
   hasScrollX: boolean;
   hasPinnedRight: boolean;
+  hasPinnedColumns: boolean;
   loadingOverlayVariant: GridLoadingOverlayVariant | null;
   overlayType: GridOverlayType;
 };
@@ -56,7 +57,7 @@ const Scroller = styled('div', {
     const { ownerState } = props;
     return [styles.virtualScroller, ownerState.hasScrollX && styles['virtualScroller--hasScrollX']];
   },
-})<{ ownerState: OwnerState }>({
+})<{ ownerState: OwnerState }>(({ ownerState }) => ({
   position: 'relative',
   height: '100%',
   flexGrow: 1,
@@ -74,9 +75,18 @@ const Scroller = styled('div', {
 
   // See https://github.com/mui/mui-x/issues/10547
   zIndex: 0,
-});
+
+  // Prevent overscroll bounce from revealing content behind pinned column shadows on macOS.
+  ...(ownerState.hasPinnedColumns && {
+    overscrollBehavior: 'none',
+  }),
+}));
 
 const hasPinnedRightSelector = (apiRef: RefObject<GridApiCommunity>) =>
+  apiRef.current.state.dimensions.rightPinnedWidth > 0;
+
+const hasPinnedColumnsSelector = (apiRef: RefObject<GridApiCommunity>) =>
+  apiRef.current.state.dimensions.leftPinnedWidth > 0 ||
   apiRef.current.state.dimensions.rightPinnedWidth > 0;
 
 export interface GridVirtualScrollerProps {
@@ -89,6 +99,7 @@ function GridVirtualScroller(props: GridVirtualScrollerProps) {
   const hasScrollY = useGridSelector(apiRef, gridHasScrollYSelector);
   const hasScrollX = useGridSelector(apiRef, gridHasScrollXSelector);
   const hasPinnedRight = useGridSelector(apiRef, hasPinnedRightSelector);
+  const hasPinnedColumns = useGridSelector(apiRef, hasPinnedColumnsSelector);
   const hasBottomFiller = useGridSelector(apiRef, gridHasBottomFillerSelector);
   const { overlayType, loadingOverlayVariant } = useGridOverlays(apiRef, rootProps);
   const Overlay = rootProps.slots?.[overlayType];
@@ -96,6 +107,7 @@ function GridVirtualScroller(props: GridVirtualScrollerProps) {
     classes: rootProps.classes,
     hasScrollX,
     hasPinnedRight,
+    hasPinnedColumns,
     overlayType,
     loadingOverlayVariant,
   };
